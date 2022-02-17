bbd965fb699281508af6e0b44d3b10ade0b1b21d
geethagunaraj <gunarajgeetha@gmail.com>
2022-02-17 13:06:27 +0530

    First Task

---
+++ src/org/github/GitHub.java
@@ -0,0 +1,25 @@
+package org.github;
+
+public class GitHub {
+	private void one() {
+System.out.println("enter number 1");
+	}
+	private void two() {
+System.out.println("enter number 2");
+	}
+	private void three() {
+System.out.println("enter number 3");
+	}
+	private void four() {
+System.out.println("enter number 4");
+	}
+	public static void main(String[] args) {
+		GitHub g = new GitHub();
+		g.one();
+		g.two();
+		g.three();
+		g.four();
+		
+	}
+
+}
